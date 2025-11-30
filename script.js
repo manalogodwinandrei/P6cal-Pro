64c587efead8e19a2b46cfc06240c373cbe178b2
Godwin Manalo <154504295+manalogodwinandrei@users.noreply.github.com>
2025-12-01 01:37:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -617,7 +617,7 @@ function sendMessage() {
 
 async function callGemini(message) {
     try {
-        const response = await fetch('http://localhost:3000/api/chat', {
+        const response = await fetch('/api/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
